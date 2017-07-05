087088c7c3fc86e0db47e8fdd8a0fee8f9a25fcd
Zorgys <grossmeyster1@gmail.com>
2017-07-05 18:00:13 +0700

    Сделал случайное появление шариков, начал реализовывать "Прилипание" шариковк башне.

---
+++ Ice Creem/Assets/Scripts/BallFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFall : MonoBehaviour {
+
+
+    private Rigidbody2D rb;
+    private Collider2D coll;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "IceCream")
+        {
+            rb.velocity = Vector2.zero;
+            rb.isKinematic = true;
+
+            coll.isTrigger = false;
+            gameObject.tag = "IceCream";            
+        }
+
+    }
+
+}
---
+++ Ice Creem/Assets/Scripts/SpawnBalls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalls : MonoBehaviour {
+
+    public List<GameObject> Balls;
+    public Transform SpawnPos;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            int RandomValue;
+            RandomValue = Random.Range(0, 10);
+            Instantiate(Balls[RandomValue], SpawnPos.position, Quaternion.identity);
+        }
+    }
+}
