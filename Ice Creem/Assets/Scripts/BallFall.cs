fe74431bc613c1e3a798f78269be4657a7651358
Zorgys <grossmeyster1@gmail.com>
2017-07-12 15:39:51 +0700

    no message

--- Ice Creem/Assets/Scripts/BallFall.cs
+++ Ice Creem/Assets/Scripts/BallFall.cs
@@ -2,29 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallFall : MonoBehaviour {
-
-
+public class BallFall : MonoBehaviour
+{
+    private GameObject Excellent;
+    private bool ex1 = false, ex2 = false, excellent = false;
+    private bool DontIceCream = true;
     private Rigidbody2D rb;
-    private Collider2D coll;
 
     private void Start()
     {
+        Excellent = GameObject.Find("Excellent");
         rb = GetComponent<Rigidbody2D>();
-        coll = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "IceCream")
-        {
-            rb.velocity = Vector2.zero;
-            rb.isKinematic = true;
 
-            coll.isTrigger = false;
-            gameObject.tag = "IceCream";            
+        if (DontIceCream && collision.name == "Ex1")
+        {
+            ex1 = true;
         }
+
+        if (DontIceCream && collision.name == "Ex2")
+        {
+            ex2 = true;
+        }
+
+        if (DontIceCream && ex2 && ex1)
+        {
+            excellent = true;
+            DontIceCream = false;
+        }
+
 
     }
 
+    private void Update()
+    {
+        if (excellent)
+        {
+            rb.velocity = Vector2.zero;
+            Excellent.transform.position = gameObject.transform.position;
+
+
+        }
+    }
 }
